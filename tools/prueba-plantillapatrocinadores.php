88a06541c6c386ec0b28196eef2daf83f3c2b694
pacolanz <paco.lanzuela@volunfair.org>
2020-12-10 13:42:11 +0100

    Cambios menores en plantilla patrocinadores

--- tools/prueba-plantillapatrocinadores.php
+++ tools/prueba-plantillapatrocinadores.php
@@ -24,7 +24,11 @@ escribe_cabecera();
                              <img src="assets/base/img/volunfair/patrocinadores/logo-airbus.png" title ="Airbus" alt="Airbus" style="padding-top: 20%;" width="100%" />
                         </a>
                     </div>
+                    <p>
+                    As a company, specialized in design, manufacture and maintenance of the rolling stock, Talgo has the following key success factors: innovation, sustainability, safety, quality, competitiveness and full custom approach to every project.
+                    </p>
                  </div>            
+                            
                             
                 <div class="col-md-3">
                     <div class="item">
@@ -32,6 +36,9 @@ escribe_cabecera();
                             <img src="assets/base/img/volunfair/patrocinadores/logo-HAD.png" title ="Hatch a Dream - Ahorra y recauda para tu voluntariado" alt="Hatch a Dream - Ahorra y recauda para tu voluntariado"  width="100%" />
                         </a>
                      </div>
+                     <p>
+                     As a company, specialized in design, manufacture and maintenance of the rolling stock, Talgo has the following key success factors: innovation, sustainability, safety, quality, competitiveness and full custom approach to every project.
+                    </p>
                  </div> 
                  
                  <div class="col-md-3">
@@ -40,6 +47,9 @@ escribe_cabecera();
                             <img src="assets/base/img/volunfair/patrocinadores/logoE.jpg" title ="ETSII Madrid" alt="ETSII Madrid"  width="100%" />
                         </a>
                      </div>
+                     <p>
+                     As a company, specialized in design, manufacture and maintenance of the rolling stock, Talgo has the following key success factors: innovation, sustainability, safety, quality, competitiveness and full custom approach to every project.
+                    </p>
                 </div>
      
                 <div class="col-md-3">
@@ -48,6 +58,9 @@ escribe_cabecera();
                             <img src="assets/base/img/volunfair/patrocinadores/logo-talgo.png" title ="Talgo" alt="Talgo" style="padding-top: 10%; padding-left: 10%;" width="90%" />
                         </a>
                     </div>
+                    <p>
+                    As a company, specialized in design, manufacture and maintenance of the rolling stock, Talgo has the following key success factors: innovation, sustainability, safety, quality, competitiveness and full custom approach to every project.
+                    </p>
                 </div>
             </div>
         </div>
@@ -66,12 +79,18 @@ escribe_cabecera();
                         <a href="http://www.etsii.upm.es/" target="_blank">
                             <img src="assets/base/img/volunfair/patrocinadores/logoE.jpg" title ="ETSII Madrid" alt="ETSII Madrid"  width="100%" />
                         </a>
+                    <p>
+                    As a company, specialized in design, manufacture and maintenance of the rolling stock, Talgo has the following key success factors: innovation, sustainability, safety, quality, competitiveness and full custom approach to every project.
+                    </p>
                 </div>
                 <div class="col-md-5">
                         <bold><h3 class=" c-center">Talgo</h3></bold>
                         <a href="https://www.talgo.com/" target="_blank">
                             <img src="assets/base/img/volunfair/patrocinadores/logo-talgo.png" title ="Talgo" alt="Talgo" style="padding-top: 10%; padding-left: 10%;" width="90%" />
                         </a>
+                    <p>
+                    As a company, specialized in design, manufacture and maintenance of the rolling stock, Talgo has the following key success factors: innovation, sustainability, safety, quality, competitiveness and full custom approach to every project.
+                    </p>
                 </div>
                 <div class="col-md-1">
                 <a>
--- volunfairOnline/o-plantillapatrocinadores.php
+++ volunfairOnline/o-plantillapatrocinadores.php
@@ -8,17 +8,27 @@ include ('./scripts/o-config.php');
 escribe_cabecera();
 
 ?>
-    <!-- BEGIN: PAGE CONTAINER -->
-    <div class="c-content-box c-size-md c-bg-white">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-3">
+        <div class="c-content-title-1">
+        <h3 class="c-font-uppercase c-center c-font-bold">Patrocinadores</h3>
+        <div class="c-line-center c-theme-bg"></div>
+        </div>
+        
+        
+        <!-- BEGIN: PAGE CONTAINER -->
+        <!-- MODELO PATROCINADORES NO MUY GRANDES --> 
+        <div class="c-content-box c-size-md c-bg-white">     
+            <div class="container">
+                 <div class="col-md-3">
                     <div class="item">
                         <a href="https://www.airbus.com/" target="_blank">
                              <img src="assets/base/img/volunfair/patrocinadores/logo-airbus.png" title ="Airbus" alt="Airbus" style="padding-top: 20%;" width="100%" />
                         </a>
                     </div>
+                    <p>
+                    As a company, specialized in design, manufacture and maintenance of the rolling stock, Talgo has the following key success factors: innovation, sustainability, safety, quality, competitiveness and full custom approach to every project.
+                    </p>
                  </div>            
+                            
                             
                 <div class="col-md-3">
                     <div class="item">
@@ -26,6 +36,9 @@ escribe_cabecera();
                             <img src="assets/base/img/volunfair/patrocinadores/logo-HAD.png" title ="Hatch a Dream - Ahorra y recauda para tu voluntariado" alt="Hatch a Dream - Ahorra y recauda para tu voluntariado"  width="100%" />
                         </a>
                      </div>
+                     <p>
+                     As a company, specialized in design, manufacture and maintenance of the rolling stock, Talgo has the following key success factors: innovation, sustainability, safety, quality, competitiveness and full custom approach to every project.
+                    </p>
                  </div> 
                  
                  <div class="col-md-3">
@@ -34,6 +47,9 @@ escribe_cabecera();
                             <img src="assets/base/img/volunfair/patrocinadores/logoE.jpg" title ="ETSII Madrid" alt="ETSII Madrid"  width="100%" />
                         </a>
                      </div>
+                     <p>
+                     As a company, specialized in design, manufacture and maintenance of the rolling stock, Talgo has the following key success factors: innovation, sustainability, safety, quality, competitiveness and full custom approach to every project.
+                    </p>
                 </div>
      
                 <div class="col-md-3">
@@ -42,10 +58,50 @@ escribe_cabecera();
                             <img src="assets/base/img/volunfair/patrocinadores/logo-talgo.png" title ="Talgo" alt="Talgo" style="padding-top: 10%; padding-left: 10%;" width="90%" />
                         </a>
                     </div>
+                    <p>
+                    As a company, specialized in design, manufacture and maintenance of the rolling stock, Talgo has the following key success factors: innovation, sustainability, safety, quality, competitiveness and full custom approach to every project.
+                    </p>
                 </div>
+            </div>
         </div>
-    </div>
-    <!-- END: PAGE CONTAINER -->
+        
+        <br /><br />
+
+        <!-- MODELO PATROCINADORES GRANDES --> 
+        <div class="c-content-box c-size-md c-bg-white">    
+            <div class="container">        
+                <div class="col-md-1"> 
+                <a>
+                </a>
+                </div>
+                <div class="col-md-5">
+                        <bold><h3 class="c-center">Talgo</h3></bold>
+                        <a href="http://www.etsii.upm.es/" target="_blank">
+                            <img src="assets/base/img/volunfair/patrocinadores/logoE.jpg" title ="ETSII Madrid" alt="ETSII Madrid"  width="100%" />
+                        </a>
+                    <p>
+                    As a company, specialized in design, manufacture and maintenance of the rolling stock, Talgo has the following key success factors: innovation, sustainability, safety, quality, competitiveness and full custom approach to every project.
+                    </p>
+                </div>
+                <div class="col-md-5">
+                        <bold><h3 class=" c-center">Talgo</h3></bold>
+                        <a href="https://www.talgo.com/" target="_blank">
+                            <img src="assets/base/img/volunfair/patrocinadores/logo-talgo.png" title ="Talgo" alt="Talgo" style="padding-top: 10%; padding-left: 10%;" width="90%" />
+                        </a>
+                    <p>
+                    As a company, specialized in design, manufacture and maintenance of the rolling stock, Talgo has the following key success factors: innovation, sustainability, safety, quality, competitiveness and full custom approach to every project.
+                    </p>
+                </div>
+                <div class="col-md-1">
+                <a>
+                </a>
+                </div>
+            </div>
+        </div>
+        
+        
+        
+        <!-- END: PAGE CONTAINER -->
 
 <?php
 
