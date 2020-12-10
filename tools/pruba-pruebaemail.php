abff70a83347a1454420d0984e58e608785bc1af
pacolanz <paco.lanzuela@volunfair.org>
2020-12-10 20:58:20 +0100

    primero botón correo

---
+++ tools/pruba-pruebaemail.php
@@ -0,0 +1,35 @@
+<?php
+
+
+// --- Archivo con las funciones de configuración (º, pie, ...)
+include ('./scripts/o-config.php');
+
+// --- Escribimos las cabeceras
+escribe_cabecera();
+<br /><br />
+<br /><br />
+
+?>
+    <div class="c-content-title-1">
+    <h3 class="c-font-uppercase c-center c-font-bold">Patrocinadores</h3>
+    <div class="c-line-center c-theme-bg"></div>
+    </div>
+
+
+    <!-- BEGIN: PAGE CONTAINER -->
+    <!-- BOTÓN -->
+    <div class="c-content-box c-size-md c-bg-white">     
+        <div class="container">
+            <label for="email">Introduce tu email:</label>
+            <input type="email" id="email">
+        
+        </div>
+    </div>
+
+
+<?php
+
+// --- Escribimos el pie de página (el archivo de configuración ya está incluido)
+escribe_pie();
+
+?>
